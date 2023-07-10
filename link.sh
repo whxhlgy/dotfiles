f34caace3691e312c3b092e1b26a6b445c46d219
Junjie-Zhong <zjj476778843@gmail.com>
2023-07-10 21:31:18 +0800

    my dot-files inititation

--- link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-ln -sf ~/dotfile/.vimrc ~/.vimrc
-ln -sf ~/dotfile/.zshrc ~/.zshrc
-ln -sf ~/dotfile/.gitconfig ~/.gitconfig
-ln -sf ~/dotfile/.vim ~/.vim
-ln -sf ~/dotfile/.oh-my-zsh ~/.oh-my-zsh
