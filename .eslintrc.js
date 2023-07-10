0bd53a08d267c9143aa27674e587e93ecafc185d
Junjie-Zhong <zjj476778843@gmail.com>
2023-07-10 21:31:18 +0800

    my dot-files inititation

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // "env": {
+  //     "browser": true,
+  //     "es2021": true
+  // },
+  "extends": "semistandard",
+  "rules" : {
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "never",
+      "asyncArrow": "never"
+    }],
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# 命令行工具
-* tldr (a more simplfy man page)
+# My Configuration file
+
+## ESLint
+
