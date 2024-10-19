a49aeddc6da318866fda77d1a81238e92b0e103a
Zhong, Junjie <junjie.zhong@intel.com>
2024-10-19 19:51:46 +0800

    add lazygit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -186,6 +186,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('v', '<leader>sv', "y<cmd>exec 'Telescope live_grep default_text=' . escape(@0, ' ')<cr>", { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
